ee67ca3ffc539fe2ea66b4a1d43f90da77d6e98b
Rizky Alam <sketsacartoon@gmail.com>
2025-03-10 08:56:32 +0700

    fix: remove special symbols from slug

--- src/application/services/post.services.ts
+++ src/application/services/post.services.ts
@@ -4,98 +4,98 @@ import "reflect-metadata";
 import slugify from "slugify";
 import { DBError, NotFoundError } from "../../infrastructure/entity/error";
 import type {
-  CreatePost,
-  IPost,
-  UpdatePost,
+	CreatePost,
+	IPost,
+	UpdatePost,
 } from "../../infrastructure/entity/interface";
 import { TYPES } from "../../infrastructure/entity/types";
 
 @injectable()
 export class PostServices {
-  constructor(@inject(TYPES.postRepo) private postRepo: IPost) {}
+	constructor(@inject(TYPES.postRepo) private postRepo: IPost) {}
 
-  async getAll(userId: string) {
-    try {
-      const posts = await this.postRepo.getAll(userId);
-      return posts;
-    } catch (error) {
-      if (error instanceof DBError) {
-        throw error;
-      }
-      throw error;
-    }
-  }
+	async getAll(userId: string) {
+		try {
+			const posts = await this.postRepo.getAll(userId);
+			return posts;
+		} catch (error) {
+			if (error instanceof DBError) {
+				throw error;
+			}
+			throw error;
+		}
+	}
 
-  async getOne(id: string) {
-    try {
-      const post = await this.postRepo.getOne(id);
-      return post;
-    } catch (error) {
-      if (error instanceof NotFoundError) {
-        throw error;
-      }
-      throw error;
-    }
-  }
+	async getOne(id: string) {
+		try {
+			const post = await this.postRepo.getOne(id);
+			return post;
+		} catch (error) {
+			if (error instanceof NotFoundError) {
+				throw error;
+			}
+			throw error;
+		}
+	}
 
-  async create(data: CreatePost) {
-    try {
-      const options = {
-        lower: true,
-        remove: /[*+~.()'"!:@]/g,
-        strict: true,
-      };
+	async create(data: CreatePost) {
+		try {
+			const options = {
+				lower: true,
+				remove: /[*+~.()'"!:@]/g,
+				strict: true,
+			};
 
-      const titleSlug = slugify(data.slug, options);
-      const generateCode = crypto.randomInt(10000, 99999).toString();
+			const titleSlug = slugify(data.slug, options);
+			const generateCode = crypto.randomInt(10000, 99999).toString();
 
-      data.slug = `${generateCode}-${titleSlug}`;
+			data.slug = `${generateCode}-${titleSlug}`;
 
-      const newPost = await this.postRepo.create(data);
+			const newPost = await this.postRepo.create(data);
 
-      return newPost;
-    } catch (error) {
-      if (error instanceof DBError) {
-        throw error;
-      }
-      throw error;
-    }
-  }
+			return newPost;
+		} catch (error) {
+			if (error instanceof DBError) {
+				throw error;
+			}
+			throw error;
+		}
+	}
 
-  async update(postId: string, data: UpdatePost) {
-    try {
-      const isPostExist = await this.postRepo.getOne(postId);
+	async update(postId: string, data: UpdatePost) {
+		try {
+			const isPostExist = await this.postRepo.getOne(postId);
 
-      if (data.title && data.title !== isPostExist.title) {
-        const options = {
-          lower: true,
-          remove: /[*+~.()'"!:@]/g,
-          strict: true,
-        };
-        const titleSlug = slugify(data.title, options);
-        const generateCode = crypto.randomInt(10000, 99999).toString();
-        data.slug = `${generateCode}-${titleSlug}`;
-      }
+			if (data.title && data.title !== isPostExist.title) {
+				const options = {
+					lower: true,
+					remove: /[*+~.()'"!:@]/g,
+					strict: true,
+				};
+				const titleSlug = slugify(data.title, options);
+				const generateCode = crypto.randomInt(10000, 99999).toString();
+				data.slug = `${generateCode}-${titleSlug}`;
+			}
 
-      const updatedPost = await this.postRepo.update(postId, data);
+			const updatedPost = await this.postRepo.update(postId, data);
 
-      return updatedPost;
-    } catch (error) {
-      if (error instanceof DBError) {
-        throw error;
-      }
-      throw error;
-    }
-  }
+			return updatedPost;
+		} catch (error) {
+			if (error instanceof DBError) {
+				throw error;
+			}
+			throw error;
+		}
+	}
 
-  async delete(postId: string) {
-    try {
-      await this.postRepo.delete(postId);
-    } catch (error) {
-      if (error instanceof DBError) {
-        throw error;
-      }
-      throw error;
-    }
-  }
+	async delete(postId: string) {
+		try {
+			await this.postRepo.delete(postId);
+		} catch (error) {
+			if (error instanceof DBError) {
+				throw error;
+			}
+			throw error;
+		}
+	}
 }
